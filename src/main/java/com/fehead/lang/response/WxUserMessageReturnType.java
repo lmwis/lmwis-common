449e860c272bff4ef73f02389dcfbc70cd70cf90
lmwis <detectiveygn@163.com>
2020-11-28 16:55:55 +0800

    fix<WxUserMessageReturnType>:init

---
+++ src/main/java/com/fehead/lang/response/WxUserMessageReturnType.java
@@ -0,0 +1,20 @@
+package com.fehead.lang.response;
+
+import io.swagger.annotations.ApiModel;
+
+/**
+ * @Description: 微信接受用户消息返回类型
+ * @Author: lmwis
+ * @Date 2020-11-28 16:38
+ * @Version 1.0
+ */
+@ApiModel(value = "微信接受用户消息返回类型",parent = FeheadResponse.class)
+public class WxUserMessageReturnType extends FeheadResponse {
+
+
+    public static WxUserMessageReturnType create(Object result) {
+        WxUserMessageReturnType type = new WxUserMessageReturnType();
+        type.setData(result);
+        return type;
+    }
+}
