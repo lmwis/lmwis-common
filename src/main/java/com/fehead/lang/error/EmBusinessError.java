16986fdd641439a9fd87337024faab05487b0a5c
lmwis <detectiveygn@163.com>
2022-05-20 17:44:14 +0800

    feat: 文件上传

--- src/main/java/com/fehead/lang/error/EmBusinessError.java
+++ src/main/java/com/fehead/lang/error/EmBusinessError.java
@@ -24,6 +24,8 @@ public enum EmBusinessError implements CommonError {
     JSON_SEQUENCE_WRONG(10008,"不合法的json序列化字符串"),
     RPC_FAIL(100009,"远程服务调用异常"),
     JWT_TOKEN_EXPIRED(100010,"token 已过期"),
+    UPLOAD_FILE_EMPTY(100011,"上传文件为空"),
+    UPLOAD_FILE_SIZE_LIMIT(100012,"达到上传文件大小限制"),
 
     // 20000 开头为用户信息相关错误定义
     USER_NOT_EXIST(20001, "用户不存在"),
